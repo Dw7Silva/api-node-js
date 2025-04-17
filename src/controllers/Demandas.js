e8c10d0488ed3508ce3adb230c59b4adbe6f7d08
Dw7Silva <derick.kdd@gmail.com>
2025-04-17 11:43:29 -0300

    Mostrar registros, row

--- src/controllers/Demandas.js
+++ src/controllers/Demandas.js
@@ -3,10 +3,23 @@ const db = require('../dataBase/connection');
 module.exports = {
     async listarDemandas(request, response) {
         try {
+
+            const sql = `
+            SELECT
+                demanda_id, emp_id, amen_id, demanda_quantidade, demanda_preco_maximo,
+                demanda_data_entrega, demanda_outras_informacoes, demanda_data_publicacao, demanda_ativa = 1 AS usu_ativo
+            FROM DEMANDAS;
+                        `;
+            
+            const [rows] = await db.query(sql);
+
+            const nRegistros = rows.length;
+
             return response.status(200).json({
                 sucesso: true, 
                 mensagem: 'Lista de Demandas', 
-                dados: null
+                resultado: nRegistros,
+                dados: rows
             });
         } catch (error) {
             return response.status(500).json({
